5213538ff44e15d690d5abde752f166a50d41084
stubh505 <kaustubh.sarkar505@gmail.com>
2020-03-29 17:44:02 +0530

    Initial commit. Created components, nav and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { EventsComponent } from './events/events.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'home', component : HomeComponent },
+  { path : 'about', component : AboutComponent },
+  { path : 'blogs', component : BlogsComponent },
+  { path : 'events', component : EventsComponent },
+  { path : 'contactus', component : ContactusComponent},
+
+  { path: '', component : HomeComponent },
+  { path: "**", redirectTo : "", pathMatch : 'full' 
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FootComponent } from './foot/foot.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { EventsComponent } from './events/events.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FootComponent,
+    HomeComponent,
+    AboutComponent,
+    EventsComponent,
+    BlogsComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
