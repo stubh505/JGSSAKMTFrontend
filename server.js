60704b6af886234f876eedef7b20c546ebb6a661
stubh505 <kaustubh.sarkar505@gmail.com>
2020-04-18 12:18:50 +0530

    Improved server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/jgssakmtfrontend'));
+app.use(express.static(__dirname + '/dist/JGSSAKMTFrontend'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/jgssakmtfrontend/index.html'));
+res.sendFile(path.join(__dirname+'/dist/JGSSAKMTFrontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
