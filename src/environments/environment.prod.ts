f117e9772ee6b99a83c16db7b360edb84e1d9641
stubh505 <kaustubh.sarkar505@gmail.com>
2020-04-18 11:48:01 +0530

    Improved evn prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  api: "http://jgssakmtback.herokuapp.com/jgssakmt_backend/",
+  blogsapi: "http://jgssakmtback.herokuapp.com/jgssakmt_backend/BlogsAPI/",
+  contactapi: "http://jgssakmtback.herokuapp.com/jgssakmt_backend/ContactUsAPI/",
+  eventsapi: "http://jgssakmtback.herokuapp.com/jgssakmt_backend/EventsAPI/",
+  pagesapi: "http://jgssakmtback.herokuapp.com/jgssakmt_backend/PagesAPI/"
 };
